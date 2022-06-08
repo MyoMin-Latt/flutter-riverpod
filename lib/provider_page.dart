4572c9d1d31979aebacbd2138ba3d71745acbf74
Myo Min Latt <myominlatt.sbs@gmail.com>
2022-06-08 10:53:23 +0630

    provider: numberProvider with consumer

--- lib/provider_page.dart
+++ lib/provider_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final numberProvider = Provider<int>((ref) {
+  return 42;
+});
 
 class ProviderPage extends StatelessWidget {
   const ProviderPage({Key? key}) : super(key: key);
@@ -7,7 +12,15 @@ class ProviderPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Provider'),
+        title: const Text('Provider: Use Consumer'),
+      ),
+      body: Center(
+        child: Consumer(
+          builder: (context, ref, child) {
+            final number = ref.watch(numberProvider);
+            return Text(number.toString());
+          },
+        ),
       ),
     );
   }
