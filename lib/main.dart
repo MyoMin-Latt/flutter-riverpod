f7c5b97b50701ab87a17687ce704102055f98cc8
Myo Min Latt <myominlatt.sbs@gmail.com>
2022-06-08 10:42:12 +0630

    first push

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_flutter_riverpod/provider_page.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ProviderPage(),
+    );
+  }
+}
