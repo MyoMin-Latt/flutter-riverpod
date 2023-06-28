7eb7549cc22392d8ab8fbccf54936e050219919a
MyoMinLatt <myominlatt.sbs@gmail.com>
2023-06-28 09:37:00 +0630

    add itemCard to page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:state_flutter_riverpod/change_notifier_page.dart';
+import 'package:state_flutter_riverpod/provider_page.dart';
+import 'package:state_flutter_riverpod/provider_page2.dart';
+import 'package:state_flutter_riverpod/scope_provider_page.dart';
+import 'package:state_flutter_riverpod/state_notifier_page.dart';
+import 'package:state_flutter_riverpod/state_provider_page.dart';
 
 import 'future_stream_page.dart';
 
@@ -12,8 +18,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: FutureStreamPage(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('RiverPod'),
+        ),
+        body: ListView(
+          children: const [
+            ItemCard(title: "Provider", page: ProviderPage()),
+            ItemCard(title: "Provider2", page: ProviderPage2()),
+            ItemCard(title: "StateProvider", page: StateProviderPage()),
+            ItemCard(title: "StateNotifier", page: StateNotifierPage()),
+            ItemCard(title: "ChangeNotifier", page: ChangeNotifierPage()),
+            ItemCard(title: "ScopeProvider", page: ScopeProviderPage()),
+            ItemCard(title: "FutureStreamProvider", page: FutureStreamPage()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  final Widget page;
+  final String title;
+  const ItemCard({
+    super.key,
+    required this.page,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Card(
+        child: ListTile(
+          title: Text(title),
+          trailing: const Icon(Icons.arrow_forward_ios),
+          onTap: () => Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => page,
+          )),
+        ),
+      ),
     );
   }
 }
